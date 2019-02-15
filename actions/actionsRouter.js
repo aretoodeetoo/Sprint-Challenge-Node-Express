72938fbbc035a50ddfb5260cacdff5f5f1e1bed6
croy <roy.christie95@gmail.com>
2019-02-15 11:35:07 -0500

    Implemented GET action by ID successfully

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -4,6 +4,7 @@ const Actions = require('../data/helpers/actionModel');
 
 const router = express.Router();
 
+// Gets list of all actions
 router.get('/', async (req, res) => {
     try {
         const actions = await Actions.get();
@@ -11,6 +12,20 @@ router.get('/', async (req, res) => {
     } catch(error){
         console.log(error);
         res.status(500).json({ message: "Error retrieving list of actions"});
+    }
+})
+
+// Gets action by id
+router.get('/:id', async (req, res) => {
+    try {
+        const action = await Actions.get(req.params.id);
+        if (action){
+            res.status(200).json(action);
+        } else {
+            res.status(404).json({ message: "The action with the specified ID could not be found"});
+        }
+    } catch {
+        res.status(500).json({ message: "The action info could not be retrieved"});
     }
 })
 
