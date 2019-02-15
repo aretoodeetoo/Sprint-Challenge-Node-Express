720438c2825294689ae334ee2059b2b61c0c0c3a
croy <roy.christie95@gmail.com>
2019-02-15 13:32:49 -0500

    MVP accomplished, added cors for stretch

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const actionsRouter = require('./actions/actionsRouter');
 const projectRouter = require('./projects/projectRouter');
@@ -9,6 +10,7 @@ const projectRouter = require('./projects/projectRouter');
 // server commands
 const server = express();
 server.use(express.json());
+server.use(cors());
 server.use(helmet());
 server.use(morgan('dev'));
 
